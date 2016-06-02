df1e8567e3adb9127b60e12bb55dc8e563a42592
bc0067042 <bc0067042@ishi-pc20192992.local>
2016-06-02 17:32:11 +0900

    create

--- UikitTest2/ViewController.swift
+++ UikitTest2/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func hundle(sender: AnyObject) {
+        label.text = "Swift"
+    }
+    
+    @IBOutlet weak var label: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
